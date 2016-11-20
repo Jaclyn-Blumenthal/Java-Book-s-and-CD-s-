3bba490e83b75619f465a9c36639807fc794da5d
Jaclyn-Blumenthal <jaclyn.blumenthal619@gmail.com>
2016-11-20 12:12:19 -0500

    Add files via upload
    THIS PROGRAM WILL COUNT VOWELS IN AN ARRAY OF STRINGS

---
+++ ex7_6.java
@@ -0,0 +1,23 @@
+
+//THIS PROGRAM WILL COUNT VOWELS IN AN ARRAY OF STRINGS
+
+public class ex7_6
+{ 
+  public static void main(String[] args)
+  {
+    int count = 0, i, j;
+    String vowels= "aeiouAEIOU";
+    String[] names = {"Jaclyn", "Lauren", "Sarah" , "Taylor", "Matthew"};
+    
+    for (i=0; i < names.length; i++)
+      
+      for (j = 0; j < names[i].length(); j++)
+      
+      if (vowels.indexOf(names[i].charAt(j)) != -1)
+      count++;
+    
+    System.out.println("The number of vowels is " + count);
+      
+      
+  }
+}
